570259dc61cc91293ebf98d9462f80720b1664d4
shadowless16 <akdavid4real@gmail.com>
2025-02-08 20:13:07 +0100

    Refactor calculator input handling for improved user experience and error management

--- Calculator.py
+++ Calculator.py
@@ -1,28 +1,36 @@
-trigger = input("Do you want to perform a calculation?(Y)")
+trigger = input("Do you want to perform a calculation?(Y)").upper().strip()
 while trigger == "Y" :
- operator = input("Select (+, /, -, *)")
- Num1 = float(input("first number"))
- print("Number Accepted")
- Num2 = float(input("second number"))
- print("Number Accepted")
+    try:
+        Num1 = float(input("first number: "))
+        print("Number Accepted")
+        Num2 = float(input("second number: "))
+        print("Number Accepted")
+        operator = input("Select (+, /, -, *): ")
 
- if operator == "+":
-     Result = Num1 + Num2
-     print(Result)
-     trigger == input("Do you wish to perform another calculation?(Y)")
- elif operator == "/":
-     Result = Num1 / Num2
-     print(Result)
-     trigger == input("Do you wish to perform another calculation?(Y)")
- elif operator == "-":
-     Result = Num1 - Num2
-     print(Result)
-     trigger == input("Do you wish to perform another calculation?(Y)")
- elif operator == "*":
-     Result = Num1 * Num2
-     print(Result)
-     trigger == input("Do you wish to perform another calculation?(Y)")
- else:
-     print("This is not an operator, dumbass")
-else:
-    print(f"No calculation shall be carried out then")
+        if operator == "+":
+            Result = Num1 + Num2
+            print(Result)
+        elif operator == "/":
+            Result = Num1 / Num2
+            print(Result)
+        elif operator == "-":
+            Result = Num1 - Num2
+            print(Result)
+        elif operator == "*":
+            Result = Num1 * Num2
+            print(Result)
+        else:
+            print("This is not an operator, dumbass")
+            print(f"No calculation shall be carried out then")
+
+        trigger = input("Do you want to another perform a calculation?(Y)").upper().strip()
+    except ValueError:
+        print("Invlaid")
+
+
+#corrections made
+#for the trigger variable you werent case sensetive
+#the operator vaildator was supposed to be carried out on the spot the moment the operator is inputted not when the operation is taking place
+#you didn't handle the error for when a user enters a letter in the varibale for the numbers
+#you were supposed to call the operator varible after the numbers varible 
+#you were calling the trigger multiple times in the if blocks which causes the code to be redundant 
